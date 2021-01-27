a007d6b50fbae8ead797104c4ff082d32410e44d
dlscoccia <2054@holbertonschool.com>
2021-01-27 17:13:42 +0000

    Fixing pep8 at /views/index.py; and fixing exutable mode index.py & app.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,18 +4,20 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-classes = { "amenities": "Amenity",
-            "cities": "City",
-            "places": "Place",
-            "reviews": "Review",
-            "states": "State",
-            "users": "User"
-            }
+classes = {"amenities": "Amenity",
+           "cities": "City",
+           "places": "Place",
+           "reviews": "Review",
+           "states": "State",
+           "users": "User"
+           }
+
 
 @app_views.route('/status')
 def api_status():
     """api_status"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats')
 def stats():
