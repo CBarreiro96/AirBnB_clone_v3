b5b5e15023f7cd348697d7d315cd430da7c3648b
dlscoccia <2054@holbertonschool.com>
2021-01-26 22:16:33 -0500

    Updating index.py with new route stats; pep8 checked

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,25 @@
 """List of states"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
+classes = { "amenities": "Amenity",
+            "cities": "City",
+            "places": "Place",
+            "reviews": "Review",
+            "states": "State",
+            "users": "User"
+            }
 
 @app_views.route('/status')
 def api_status():
     """api_status"""
     return jsonify({"status": "OK"})
+
+@app_views.router('/stats')
+def stats():
+    '''retrieves the number of each objects by type'''
+    dict = {}
+    for key, value in classes.items():
+        dict[key] = storage.count(value)
+    return jsonify(dict)
