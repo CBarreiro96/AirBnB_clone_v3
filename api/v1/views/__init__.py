6f4b947efcb52c89e861b4cd1587f21b59366a51
dlscoccia <2054@holbertonschool.com>
2021-01-27 23:06:04 -0500

    Addin task 10 users with pep8

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,5 @@ from models.state import State
 from api.v1.views.states import *
 from models.city import City
 from api.v1.views.cities import *
+from models.user import User
+from api.v1.views.users import *
